1b6891c13f34a07ba4b1946a2f21753d11a91d51
Rokkit <fss92@pochta.ru>
2012-05-01 17:22:44 +0400

    Переписал на другой архиватор

--- php_script/Forms/Form_docxgen/form_creator.php
+++ php_script/Forms/Form_docxgen/form_creator.php
@@ -1,9 +1,12 @@
 <?php
+
 $root = $_SERVER['DOCUMENT_ROOT'];
 require '../../auth.php';
 require_once '../../function.php';
 require_once '../../Student/Student.php';
 require '../../../php_script/djpate-docxgen/phpDocx.php';
+//require '../../../php_script/includes/ZipArchive.php';
+require_once "../../djpate-docxgen/lib/pclzip.lib.php";
 
 if(isset($_GET['id'])) {//формируем документ
     $fspodb = connectToFspoDB();
@@ -44,6 +47,7 @@ if(isset($_GET['id'])) {//формируем документ
                 //creating documents
                 $files=array();
                 $directory="files/";
+                $zipdir="files/archives/";
                 foreach($ids as $id) {
                     $id = parseNumSql($id);
                     $student=Student::getStudentById($id);
@@ -56,16 +60,48 @@ if(isset($_GET['id'])) {//формируем документ
         $filename = $directory.encodestring($student->name."_".$student->last_name)."_".$student->group.".docx";
 
         @$phpdocx->save($filename);//тут вылезают ошибки на денаид но они никак не влияют
-
+        $files[]=$filename;
                 }
                 $valid_files = array();
-                /* for every image */
-                foreach($file as $files) {
-                /* if the image exists */
+                /* for every file */
+                foreach($files as $file) {
+                /* if the file exists */
                     if(file_exists($file)) {
                     /* add it to our good file list */
                     $valid_files[] = $file;
                     }
+                }
+                if(count($valid_files)) {
+                    $zipfilename=time().".zip";
+                    $path = $zipdir.$zipfilename;
+
+//                    $zip = new ZipArchive();
+//                    if($zip->open($path, ZIPARCHIVE::CREATE)!==true)
+//                            return false;
+                        $zip=new PclZip($path);
+
+
+
+                /* add files */
+                foreach($valid_files as $file) {
+                    $zip->add($file);
+                }
+
+                if(!file_exists($path)) {  return false; }
+ /* force download zip */
+    if(ini_get('zlib.output_compression')) { ini_set('zlib.output_compression', 'Off');  }
+    header('Pragma: public');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+    header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
+    header('Cache-Control: private',false);
+    header('Content-Type: application/zip');
+    header('Content-Disposition: attachment; filename="package.zip"');
+    header('Content-Transfer-Encoding: binary');
+    header('Content-Length: '.filesize($path));
+    header('Connection: close');
+    readfile($path);
+    exit();
                 }
             }
         }
